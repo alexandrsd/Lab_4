529b26f9a992dfb2e32d3161a79040d614e68d01
Саша Дьячков <sashadyachkov11738@mail.ru>
2019-12-20 07:03:09 +0300

    Добавдение формулы

--- laba4/Form1.cs
+++ laba4/Form1.cs
@@ -19,15 +19,15 @@ namespace laba4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double r = Convert.ToDouble(textBox1.Text);
+            double r = Convert.ToDouble(textBox1.Text); 
             double d = ((2 * Convert.ToInt32(textBox1.Text) - 7) / (Math.Sqrt(3 * Convert.ToInt32(textBox1.Text) + 21)));
-            label1.Text = Convert.ToString(d);
+            label1.Text = Convert.ToString(d); 
 
             double d1 = (Math.Sqrt(Convert.ToInt32(textBox1.Text) / (Convert.ToInt32(textBox1.Text))) - 1);
-            label2.Text = Convert.ToString(d1);
+            label2.Text = Convert.ToString(d1); 
 
             double d2 = ((Convert.ToInt32(textBox1.Text) + 1) / (Convert.ToInt32(textBox1.Text) - 1) + Math.Sqrt(Math.Pow(Convert.ToInt32(textBox1.Text), 2)));
-            label3.Text = Convert.ToString(d2);
+            label3.Text = Convert.ToString(d2); 
         }
     }
 }
